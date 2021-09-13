0e08c8ebfec398cbe91b7528be8f5359005885a9
kaetyan <kaechan11111@gmail.com>
2021-09-13 12:41:36 +0000

    microposts

--- app/User.php
+++ app/User.php
@@ -138,7 +138,7 @@ class User extends Authenticatable
      */
     public function loadRelationshipCounts()
     {
-        $this->loadCount('microposts');
+        $this->loadCount('microposts', 'followings', 'followers');
     }
     
     
